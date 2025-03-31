5cad182fc232ac8b85af16debd4caa5d09711dac
Mia Pedersen <miap@student.uia.no>
2025-03-31 21:13:39 +0200

    added some buttons and a header to the html, and created some constants to store the data in the mjs file

--- app.mjs
+++ app.mjs
@@ -1,1 +1,6 @@
-console.log("APP");
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const addTaskButton = document.getElementById('addTaskButton');
+const deleteTaskButton = document.getElementById('deleteTaskButton');
+const editTaskButton = document.getElementById('editTaskButton');
+const completedTaskButton = document.getElementById('completedTaskButton');
