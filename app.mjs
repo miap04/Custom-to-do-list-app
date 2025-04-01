c7bd5827a4f5345edca0c07860071b94083cd521
Mia Pedersen <miap@student.uia.no>
2025-04-01 15:09:53 +0200

    Removed unnecessary code once I learned more about html, added a function to the addTaskButton which now adds the tasks to the taskList with a checkbox, edit, and delete button using the appendChild method

--- app.mjs
+++ app.mjs
@@ -1,19 +1,25 @@
-const taskInput = document.getElementById('taskInput');
-const taskList = document.getElementById('taskList');
 const addTaskButton = document.getElementById('addTaskButton');
-const deleteTaskButton = document.getElementById('deleteTaskButton');
-const editTaskButton = document.getElementById('editTaskButton');
-const completedTaskCheckbox = document.getElementById('completedTaskCheckbox');
+const taskList = document.getElementById("taskList");
 
-function saveCompletedTaskCheckboxState() {
-    localStorage.setItem('completedTaskCheckbox', completedTaskCheckbox.checked);
+let task;
+
+addTaskButton.onclick = function () {
+    task = document.getElementById('taskInput').value;
+    const newListItem = document.createElement("li");
+    const newContent = document.createTextNode(task);
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    editButton.innerHTML = "Edit";
+    deleteButton.innerHTML = "Delete";
+    newListItem.appendChild(newContent);
+    newListItem.appendChild(checkbox);
+    newListItem.appendChild(editButton);
+    newListItem.appendChild(deleteButton);
+    taskList.appendChild(newListItem);
+    saveTask();
 }
 
-function loadCompletedTaskCheckboxState() {
-    const isChecked = localStorage.getItem('completedTaskCheckbox') === 'true';
-    completedTaskCheckbox.checked = isChecked;
-}
-
-completedTaskCheckbox.addEventListener('change', saveCompletedTaskCheckboxState);
 
 loadCompletedTaskCheckboxState();
